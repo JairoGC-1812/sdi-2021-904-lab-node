22dbf2f9966db1626c63d7d22452db4fe7293d3f
jairogarciacastro <jairogcastro@hotmail.com>
2021-03-28 13:28:10 +0200

    SDI-2021-904-7.5-Vistas y motores de plantillas

--- app.js
+++ app.js
@@ -2,14 +2,15 @@
 let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
+let swig = require('swig');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 // Variables
 app.set('port', 8081)
-require("./routes/rusuarios.js")(app); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app); // (app, param1, param2, etc.)
+require("./routes/rusuarios.js")(app, swig); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app, swig); // (app, param1, param2, etc.)
 
 // lanzar el servidor
 app.listen(app.get('port'), function (){
--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,15 +1,34 @@
-module.exports = function(app) {
+module.exports = function(app, swig) {
     app.get("/canciones", function(req, res) {
-        let respuesta = "";
+        let canciones = [ {
+            "nombre" : "Blank space",
+            "precio": "1.2"
+        },{
+            "nombre" : "See you again",
+            "precio": "1.3"
+        },{
+            "nombre" : "Uptown Funk",
+            "precio": "1.1"
+        } ];
 
-        if(req.query.nombre != null)
-            respuesta +=  'Nombre:'+ req.query.nombre + '<br>';
+        let respuesta = swig.renderFile('views/btienda.html', {
+           vendedor : 'Tienda de canciones',
+           canciones : canciones
+        });
 
-        if(typeof (req.query.autor) != "undefined")
-            respuesta += 'Autor: '+ req.query.autor;
         res.send(respuesta);
-
     });
+    app.post("/cancion", function(req, res) {
+        res.send("Canción agregada:"+req.body.nombre +"<br>"
+            +" genero :" +req.body.genero +"<br>"
+            +" precio: "+req.body.precio);
+    });
+    app.get('/canciones/agregar', function (req, res) {
+        let respuesta = swig.renderFile('views/bagregar.html', {
+
+        });
+        res.send(respuesta);
+    })
     app.get('/canciones/:id', function(req, res) {
         let respuesta = 'id: ' + req.params.id;
         res.send(respuesta);
@@ -18,9 +37,5 @@ module.exports = function(app) {
         let respuesta = 'id: ' + req.params.id + '<br>'
             + 'Género: ' + req.params.genero;
         res.send(respuesta);
-    });
-
-    app.post("/cancion", function(req,res){
-       res.send("Canción agregada:" + req.body.nombre + "<br>" + " género: " + req.body.genero + "<br>" + " precio: " + req.body.precio);
     });
 };
